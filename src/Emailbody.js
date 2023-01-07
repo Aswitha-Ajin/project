95cab57068c6b466ba5a40b495f0ad16927dc64e
Aswitha-Ajin <1112aswitha@gmail.com>
2023-01-07 16:39:19 +0530

    update

--- src/Emailbody.js
+++ src/Emailbody.js
@@ -3,17 +3,32 @@ import "./CSS/emailbody.css";
 import Emailsetting from "./Emailsetting";
 import Emailtype from "./Emailtype";
 import Emailbody1 from "./Emailbody1";
+import {  collection,addDoc } from 'firebase/firestore/lite';
 import { db } from "./firebase";
 const Emailbody = () => {
     const [email,setEmail]=useState([]);
+   
+    const data1=async()=>{
+        await addDoc(collection(db,"login").
+            onSnapshot(snapshot=>{
+                      setEmail(snapshot.docs.map(doc=>({
+                         id:doc.id,
+                        data:doc.data()
+                    })))
+                    })
+     )
+    //  db.collection("login").onSnapshot(snapshot=>{
+    //         setEmail(snapshot.docs.map(doc=>({
+    //          id:doc.id,
+    //          data:doc.data()
+    //         })))
+    //     })
+    }
     useEffect(()=>{
-        db.collection("login").onSnapshot(snapshot=>{
-            setEmail(snapshot.docs.map(doc=>({
-             id:doc.id,
-             data:doc.data()
-            })))
-        })
+       data1()
+        
     },[])
+    
     console.log(email)
     return (
 
